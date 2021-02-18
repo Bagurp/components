e0727f54d71d32ad7fe27071a922f80b5e0cfc37
Pramukh Bagur <bagurp@OCI1852.attlocal.net>
2021-02-18 10:51:12 -0600

    lazy loading, create components, refactoring

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,6 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
 
+  data = [
+    { name: 'James', age: 24, job: 'Designer' },
+    { name: 'Jill', age: 30, job: 'Engineer' },
+    { name: 'Elyse', age: 29, job: 'Product Owner' }
+  ];
+
+  headers= [
+    { key: 'name', label: 'Name' },
+    { key: 'age', label: 'Age' },
+    { key: 'job', label: 'Job' }
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
