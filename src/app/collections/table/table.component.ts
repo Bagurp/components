c929d25e0d40fbafda066e1506f8c84ed3a2b94c
Pramukh Bagur <bagurp@OCI1852.attlocal.net>
2021-02-19 10:55:47 -0600

    Fix typecasting errors for table component

--- src/app/collections/table/table.component.ts
+++ src/app/collections/table/table.component.ts
@@ -7,8 +7,8 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class TableComponent implements OnInit {
   @Input('class') classNames = '';
-  @Input() data = [];
-  @Input() headers = [];
+  @Input() data = [] as any;
+  @Input() headers = [] as any;
 
   constructor() { }
 
